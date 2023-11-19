ba2dd925f1a85d3ae80b3e49db0c6fe394d52104
Maykonctba <maykonctba@gmail.com>
2023-11-19 10:07:51 -0300

    installRestyleShopfy

--- App.tsx
+++ App.tsx
@@ -1,22 +1,19 @@
+import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 
 import {Text} from './src/components/Text/Text';
+import {Button} from './src/components/Button/Button';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text italic preset="headingLarge">
-        ConnectaCWBgg
-      </Text>
-      <Text bold preset="headingMedium">
-        ConnectaCWBgg
-      </Text>
-      <Text semiBold preset="headingLarge">
-        ConnectaCWBgg
-      </Text>
-      <Text preset="headingLarge">ConnectaCWBgg</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingLarge">ConnectaCWBgg</Text>
+        <Button title="Butaozinho" />
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
